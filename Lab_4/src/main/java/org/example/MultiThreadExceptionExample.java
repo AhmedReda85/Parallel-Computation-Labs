e344b91465a3faa06807b6581dd16c187162bb1b
Ahmed Reda <AhmedRedaSayed_sd23@fcis.bsu.edu.eg>
2025-11-01 18:32:46 +0200

    Added Lab_4

---
+++ Lab_4/src/main/java/org/example/MultiThreadExceptionExample.java
@@ -0,0 +1,25 @@
+package org.example;
+
+class Worker extends Thread {
+    public void run() {
+        try {
+            System.out.println(Thread.currentThread().getName() + " started.");
+            int result = 100 / 0; // This will throw ArithmeticException
+        } catch (Exception e) {
+            System.out.println(Thread.currentThread().getName() + " caught: " + e);
+        }
+        System.out.println(Thread.currentThread().getName() + " finished.");
+    }
+}
+
+public class MultiThreadExceptionExample {
+    public static void main(String[] args) {
+        Worker t1 = new Worker();
+        Worker t2 = new Worker();
+        Worker t3 = new Worker();
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
---
+++ Lab_4/src/main/java/org/example/RaceConditionExample.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class RaceConditionExample {
+
+    static int counter = 0; // Shared resource
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new MyTask(), "Thread-1");
+        Thread t2 = new Thread(new MyTask(), "Thread-2");
+
+        t1.start();
+        t2.start();
+    }
+
+    static class MyTask implements Runnable {
+        public void run() {
+            for (int i = 0; i < 5; i++) {
+                int current = counter;
+
+                try {
+                    // Small delay to increase overlap
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                int updated = ++counter;
+
+                System.out.println(Thread.currentThread().getName() +
+                        " -> Current: " + current + ", Updated: " + updated);
+            }
+        }
+    }
+}
